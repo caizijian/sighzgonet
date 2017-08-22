1d360f1bbb33fea20af0ef519831ccd56972acf2
GN <353827338@qq.com>
2017-08-22 12:10:04 +0800

    [Add_showallgame_2017_08_22]       GN 353827338@qq.com
    1.丑陋地显示所有比赛

---
+++ DotNet_vs2015_GO_beta_1/uglyallgame.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="uglyallgame.aspx.cs" Inherits="DotNet_vs2015_GO_beta_1.uglyallgame" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="CheckBox1" runat="server" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+        <asp:Button ID="Button1" runat="server" Text="选择" OnClick="Button1_Click" />
+    </form>
+</body>
+</html>
---
+++ DotNet_vs2015_GO_beta_1/uglyallgame.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotNet_vs2015_GO_beta_1
+{
+    public partial class uglyallgame : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            showallcompetition();
+        }
+
+        protected void showallcompetition()
+        {//显示该队目前的队员组成情况
+          
+            DataTable table = global.ShowAllCompetition();
+            GridView1.DataSource = table;
+            GridView1.DataKeyNames = new string[] { "id" };
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= GridView1.Rows.Count - 1; i++)
+            {
+                CheckBox cbox = (CheckBox)GridView1.Rows[i].FindControl("CheckBox1");
+                if (cbox.Checked == true)
+                {
+                    Session["competition_id"] = GridView1.DataKeys[i].Value;
+
+                }
+            }
+            Response.Write("<script>alert('选择成功')</script>");
+            Response.Redirect("~/detail/Game_details.aspx");
+        }
+    }
+}
