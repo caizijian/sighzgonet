ac096b1d6a037877d53dc9a9bec1bd1dac35a37b
Leo829 <154893653@qq.com>
2017-08-23 16:49:22 +0800

    ljj add shangchuang.aspx

---
+++ DotNet_vs2015_GO_beta_1/shangchuang.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="shangchuang.aspx.cs" Inherits="DotNet_vs2015_GO_beta_1.shangchuang" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 403px;
+        }
+    </style>
+</head>
+<body style="height: 570px">
+    <form id="form1" runat="server">
+    <div class="auto-style1">
+    
+                                <asp:FileUpload ID="pic_upload" runat="server" Height="27px" Width="73px" />
+&nbsp;&nbsp;&nbsp;
+                                     <asp:Button ID="btn_upload" runat="server"  Text="上传" onclick="btn_upload_Click" Height="26px" Width="63px"/>  
+                                &nbsp;&nbsp; <asp:Label ID="lbl_pic" runat="server" Text="请上传文件" BorderStyle="None" Font-Size="XX-Small" Height="16px" Width="186px"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ DotNet_vs2015_GO_beta_1/shangchuang.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotNet_vs2015_GO_beta_1
+{
+    public partial class shangchuang : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btn_upload_Click(object sender, EventArgs e)
+        {
+            if (pic_upload.HasFile)//验证是否包含文件
+            {
+                //取得文件的扩展名,并转换成小写
+                string fileExtension = Path.GetExtension(pic_upload.FileName).ToLower();
+                //对上传文件的大小进行检测，限定文件最大不超过8M
+                if (pic_upload.PostedFile.ContentLength < 8192000)
+                {
+                    string filepath = "/上传文件/";
+                    if (Directory.Exists(Server.MapPath(filepath)) == false)//如果不存在就创建file文件夹
+                    {
+                        Directory.CreateDirectory(Server.MapPath(filepath));
+                    }
+                    string virpath = filepath + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss-fffffff") + Session["uid"] + fileExtension;//;//这是存到服务器上的虚拟路径
+                    string mappath = Server.MapPath(virpath);//转换成服务器上的物理路径
+                    pic_upload.PostedFile.SaveAs(mappath);//保存图片
+                    //清空提示
+                    lbl_pic.Text = "上传成功";
+                }
+                else
+                {
+                    lbl_pic.Text = "文件大小超出8M！请重新选择！";
+                    return;
+                }
+            }
+            else
+            {
+                lbl_pic.Text = "请选择要上传的附件！";
+                return;
+            }
+
+        }
+    }
+}
